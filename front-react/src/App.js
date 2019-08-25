ced939e5a6be9c7d56e7646b7ac007531c6d1a24
manuel <manuelczs@outlook.com>
2019-08-25 00:40:00 -0300

    loop fixed

--- front-react/src/App.js
+++ front-react/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: '',
+      value: 'None',
       prevPrice: 0,
       currentPrice: 0,
       percentilValue: 0,
@@ -27,23 +27,29 @@ class App extends Component {
 
   componentDidUpdate() {
     let symbol = this.state.value;
-    axios.get(`http://localhost:5000/local/getdata/${symbol}`)
-      .then(res => {
-        let objectData = res.data        
-        let arrayData = Object.keys(objectData["Time Series (Daily)"]).map(i => objectData["Time Series (Daily)"][i])
-        // Get current day values
-        let today = arrayData[arrayData.length - 1]
-        // Get yesterday values
-        //let yesterday = arrayData[arrayData.length - 2]
 
-        this.setState({
-          open: today['1. open'],
-          high: today['2. high'],
-          low: today['3. low'],
-          close: today['4. close']
+    if(symbol !== '') {
+
+      axios.get(`http://localhost:5000/local/getdata/${symbol}`)
+        .then(res => {
+          let objectData = res.data        
+          let arrayData = Object.keys(objectData["Time Series (Daily)"]).map(i => objectData["Time Series (Daily)"][i])
+          // Get current day values
+          let today = arrayData[arrayData.length - 1]
+          // Get yesterday values
+          // let yesterday = arrayData[arrayData.length - 2]
+          // Get last value yesterday
+
+          this.setState({
+            open: parseFloat(today['1. open']).toFixed(2),
+            high: parseFloat(today['2. high']).toFixed(2),
+            low: parseFloat(today['3. low']).toFixed(2),
+            close: parseFloat(today['4. close']).toFixed(2),
+            value: ''
+          })
+          
         })
-        
-      })
+    }
   }
 
   handleChange(event) {
@@ -53,7 +59,7 @@ class App extends Component {
   render() {
     return (
       <div className="jumbotron float-div">
-        <form onChange={this.handleChange}>
+        <form onChange={this.handleChange} className="form-style">
           <select>
             <option value="FB">Facebook</option>
             <option value="GOOGL">Google</option>
@@ -63,13 +69,12 @@ class App extends Component {
           </select>
         </form>
         <div>
-          <h2>Symbol: {this.state.symbol}</h2>
-          <h2>Open: {this.state.open}</h2>
-          <h2>High: {this.state.high}</h2>
-          <h2>Low: {this.state.low}</h2>
-          <h2>Close: {this.state.close}</h2>
+          <h3>Symbol: {this.state.value}</h3>
+          <h2>Open: USD {this.state.open}</h2>
+          <h2>High: USD {this.state.high}</h2>
+          <h2>Low: USD {this.state.low}</h2>
+          <h2>Close: USD {this.state.close}</h2>
         </div>
-        <div>{this.state.value}</div>
       </div>
     )
   }
